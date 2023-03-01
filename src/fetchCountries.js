da50893e9625c8fb66a4d787bcafb682d58e1f2f
Oleg <112899765+OlegSyrovatko@users.noreply.github.com>
2023-03-01 16:54:01 +0200

    updated country

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,15 +1,26 @@
 import Notiflix from 'notiflix';
 const userList = document.querySelector(".country-info");
 
-const fetchCountries = function (value) {
+const fetchCountries = function (query) {
 
-    fetch(`https://restcountries.com/v3.1/name/${value}?fields=name,capital,population,flags,languages`)
-  
-        .then((response) => response.json())
+    let q = query.trim();
+    if (q.length === 0) {
+        userList.innerHTML = "";
+        return;
+    }
+
+    fetch(`https://restcountries.com/v3.1/name/${q}?fields=name,capital,population,flags,languages`)
+
+        .then(response => {
+            if (!response.ok) {
+                Notiflix.Notify.failure(`Oops, there is no country with that name`);
+            }
+            return response.json();
+        })
         .then((data) => {
             const contryCount = data.length;
             if (contryCount > 10) { Notiflix.Notify.info('Too many matches found. Please enter a more specific name.'); }
-            if (contryCount > 1 && contryCount <= 10) { 
+            if (contryCount > 1 && contryCount <= 10) {
                 let innerData = "<ul class = \"contry-list\">";
                 for (const contry of data) {
                     innerData += `<li class = \"contry-item\"> <img width=20 height=12 alt="${contry.name.common}" src="${contry.flags.svg}"> ${contry.name.common} </li>`;
@@ -17,61 +28,21 @@ const fetchCountries = function (value) {
                 innerData += "</ul>";
                 userList.innerHTML = innerData;
             }
-
-            // console.log(data[0].name.common);
-            // console.log(data[0].capital[0]);
-            // console.log(data[0].population);
-            // console.log(data[0].flags.svg);
-            // console.log(Object.values(data[0].languages)
-            //     .toString().split(",").join(", "));
-            // document.querySelector(".country-info").textContent = data[0].name.common;
-
-            //   result.innerHTML = `
-            //     <img src="${data[0].flags.svg}" class="flag-img">
-            //     <h2>${data[0].name.common}</h2>
-            //     <div class="wrapper">
-            //         <div class="data-wrapper">
-            //             <h4>Capital:</h4>
-            //             <span>${data[0].capital[0]}</span>
-            //         </div>
-            //     </div>
-            //     <div class="wrapper">
-            //         <div class="data-wrapper">
-            //             <h4>Continent:</h4>
-            //             <span>${data[0].continents[0]}</span>
-            //         </div>
-            //     </div>
-            //      <div class="wrapper">
-            //         <div class="data-wrapper">
-            //             <h4>Population:</h4>
-            //             <span>${data[0].population}</span>
-            //         </div>
-            //     </div>
-            //     <div class="wrapper">
-            //         <div class="data-wrapper">
-            //             <h4>Currency:</h4>
-            //             <span>${
-            //               data[0].currencies[Object.keys(data[0].currencies)].name
-            //             } - ${Object.keys(data[0].currencies)[0]}</span>
-            //         </div>
-            //     </div>
-            //      <div class="wrapper">
-            //         <div class="data-wrapper">
-            //             <h4>Common Languages:</h4>
-            //             <span>${Object.values(data[0].languages)
-            //               .toString()
-            //               .split(",")
-            //               .join(", ")}</span>
-            //         </div>
-            //     </div>
-            //   `;
+            if (contryCount === 1) {
+                let innerData = `<h3>
+                <img width=24 height=14 alt="${data[0].name.common}" src="${data[0].flags.svg}">
+                ${data[0].name.common}
+                </h3>
+                <ul class = \"contry-list\">
+                    <li class = \"contry-item\"><b>Capital:</b>  ${data[0].capital[0]} </li>
+                    <li class = \"contry-item\"><b>Population:</b>  ${data[0].population} </li>
+                    <li class = \"contry-item\"><b>Languages:</b>  ${Object.values(data[0].languages).toString().split(",").join(", ")} </li>
+                </ul>`;
+                userList.innerHTML = innerData;
+            }
         })
-        .catch(() => {
-            //   if (countryName.length == 0) {
-            //     result.innerHTML = `<h3>The input field cannot be empty</h3>`;
-            //   } else {
-            //     result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
-            //   }
+        .catch(error => {
+            // console.log(error);
         });
 }
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import './css/styles.css';
 import debounce from "lodash.debounce";
 
-
 const DEBOUNCE_DELAY = 300;
 const box = document.querySelector("#search-box");
 
 import { fetchCountries } from "./fetchCountries.js";
 
-
 box.addEventListener("input", debounce(() => {
     fetchCountries(box.value)
 }, DEBOUNCE_DELAY));
 
-
-
-
-
-//   Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
-// },
-// ).catch(({ position, delay }) => {
-//   Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
